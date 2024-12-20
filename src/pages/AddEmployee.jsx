6a3951958faf3e510040ab2adfdf8bcc8c407f3b
monwar23 <monwar2345@gmail.com>
2024-12-20 09:39:59 +0600

    form updated for small devices

--- src/pages/AddEmployee.jsx
+++ src/pages/AddEmployee.jsx
@@ -29,7 +29,7 @@ const AddEmployee = () => {
     };
 
     return (
-        <div className='p-4 w-3/5 mx-auto'>
+        <div className='p-4 lg:w-3/5 w-4/5 mx-auto'>
         <h3 className="text-xl text-center font-medium mt-5">Add New Employee</h3>
         <form onSubmit={handleSubmit} className="mt-4">
           <div className='grid grid-cols-2 gap-5'>
