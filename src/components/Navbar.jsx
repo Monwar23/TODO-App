752bf6549d3ffd7629ed71728cde630d40ba4aba
monwar23 <monwar2345@gmail.com>
2024-12-20 17:50:05 +0600

    added app name in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,8 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="bg-slate-200 shadow-lg h-12 flex justify-center items-center">
+         <div className="bg-slate-200 shadow-lg h-12 flex justify-around items-center">
+            <h2 className="text-2xl font-semibold"><span className="text-orange-500">TODO</span> App</h2>
         <div className="flex gap-5">{navLinks}</div>
     </div>
     );
