990972c792feeb47cb3a6bac1950d93bee415357
monwar23 <monwar2345@gmail.com>
2024-12-31 09:16:41 +0600

    code clean 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,34 +12,34 @@ import useDeleteItem from "../components/useDeleteItem";
 
 
 const Home = () => {
-    // state declare
-    const [employees, setEmployees] = useLocalStorage('employees', []);
-    const [tasks] = useLocalStorage('tasks', []);
-    const [filteredEmployees, setFilteredEmployees] = useState([]);
-    const deleteItem = useDeleteItem();
-    const navigate=useNavigate()
+  // state declare
+  const [employees, setEmployees] = useLocalStorage('employees', []);
+  const [tasks] = useLocalStorage('tasks', []);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const deleteItem = useDeleteItem();
+  const navigate = useNavigate()
 
-    // data load from local Storage
-    useEffect(() => {
-        setFilteredEmployees(employees);
-    }, [employees]);
- 
-    // task count
-    const getTaskCount = (employeeId) => {
-        return tasks.filter(task => task.employeeId === employeeId && task.status !== 'Completed').length;
-    };
+  // data load from local Storage
+  useEffect(() => {
+    setFilteredEmployees(employees);
+  }, [employees]);
 
-     // Redirect to Leave List when toggling
-    const redirectToLeaveList = () => {
-      toast.info('Manage employee status in the Leave List.');
-      setTimeout(() => navigate('/leaves'), 2000);
+  // task count
+  const getTaskCount = (employeeId) => {
+    return tasks.filter(task => task.employeeId === employeeId && task.status !== 'Completed').length;
   };
 
-    // delete operation
-    const deleteEmployee = (id) => {
-      const updatedEmployees = deleteItem(employees, setEmployees, id, "employees", "Employee Deleted Successfully!");
-      setFilteredEmployees(updatedEmployees); 
-    };
+  // Redirect to Leave List when toggling
+  const redirectToLeaveList = () => {
+    toast.info('Manage employee status in the Leave List.');
+    setTimeout(() => navigate('/leaves'), 2000);
+  };
+
+  // delete operation
+  const deleteEmployee = (id) => {
+    const updatedEmployees = deleteItem(employees, setEmployees, id, "employees", "Employee Deleted Successfully!");
+    setFilteredEmployees(updatedEmployees);
+  };
 
 
   const columns = [
@@ -48,55 +48,57 @@ const Home = () => {
     { header: "Email", accessor: (row) => row.email },
     { header: "Phone", accessor: (row) => row.phone },
     { header: "Task Assigned", accessor: (row) => getTaskCount(row.id) },
-    { header: "Status", accessor: (row) =>(
-      <button
+    {
+      header: "Status", accessor: (row) => (
+        <button
           onClick={redirectToLeaveList}
           className={`btn px-2 py-1 rounded-lg ${row.activeStatus === "Available" ? "bg-green-500 text-white" : "bg-orange-500 text-white"
-              }`}
-      >
+            }`}
+        >
           {row.activeStatus}
-      </button>
-  ) },
+        </button>
+      )
+    },
   ];
 
-    return (
-        <div className="mt-5">
-            <h2 className="text-center text-2xl font-medium">Employee List</h2>
+  return (
+    <div className="mt-5">
+      <h2 className="text-center text-2xl font-medium">Employee List</h2>
 
-            {/* Input box for search*/}
-            <SearchBar
-             data={employees}
-             onFilter={setFilteredEmployees}
-             keys={["name", "id", "email", "phone", "designation"]}
-             placeholder="Search by ID, Name, Email or Phone"
-            />
+      {/* Input box for search*/}
+      <SearchBar
+        data={employees}
+        onFilter={setFilteredEmployees}
+        keys={["name", "id", "email", "phone", "designation"]}
+        placeholder="Search by ID, Name, Email or Phone"
+      />
 
-            {/* table of employees details */}
-            {filteredEmployees.length > 0 ? (
-                <Table
-                columns={columns}
-                data={filteredEmployees}
-                renderActions={(employee) => (
-                  <>
-                    <Link to={`/addEmployee/${employee.id}`} className="text-orange-500">
-                      <FaRegEdit />
-                    </Link>
-                    <button
-                      onClick={() => deleteEmployee(employee.id)}
-                      className="text-orange-500 ml-2"
-                    >
-                      <RiDeleteBin5Fill />
-                    </button>
-                  </>
-                )}
-              />
-            ) : (
-                <NoData></NoData>
-                )}
-            <ToastContainer></ToastContainer>
-        </div>
+      {/* table of employees details */}
+      {filteredEmployees.length > 0 ? (
+        <Table
+          columns={columns}
+          data={filteredEmployees}
+          renderActions={(employee) => (
+            <>
+              <Link to={`/addEmployee/${employee.id}`} className="text-orange-500">
+                <FaRegEdit />
+              </Link>
+              <button
+                onClick={() => deleteEmployee(employee.id)}
+                className="text-orange-500 ml-2"
+              >
+                <RiDeleteBin5Fill />
+              </button>
+            </>
+          )}
+        />
+      ) : (
+        <NoData></NoData>
+      )}
+      <ToastContainer></ToastContainer>
+    </div>
 
-    );
+  );
 };
 
 export default Home;
