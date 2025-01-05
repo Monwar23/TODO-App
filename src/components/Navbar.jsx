ec70808050ac3883ef59f1df8a5f12f956f928c4
monwar23 <monwar2345@gmail.com>
2025-01-05 21:07:09 +0600

    added some clear code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,38 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    const navLinks = 
-    <>
-        <NavLink className={({ isActive }) =>
-            isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
-        } to="/">Home</NavLink>
+    const navLinks =
+        <>
+            <NavLink className={({ isActive }) =>
+                isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
+            } to="/">Home</NavLink>
 
-        <NavLink className={({ isActive }) =>
-            isActive ? 'text-orange-500  font-semibold' : 'font-semibold hover:text-orange-500'
-        } to="/addEmployee">Add Employee</NavLink>
+            <NavLink className={({ isActive }) =>
+                isActive ? 'text-orange-500  font-semibold' : 'font-semibold hover:text-orange-500'
+            } to="/addEmployee">Add Employee</NavLink>
 
-        <NavLink className={({ isActive }) =>
-            isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
-        } to="/tasks">Tasks</NavLink>
+            <NavLink className={({ isActive }) =>
+                isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
+            } to="/tasks">Tasks</NavLink>
 
-        <NavLink className={({ isActive }) =>
-            isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
-        } to="/assignTasks">Assign Tasks</NavLink>
+            <NavLink className={({ isActive }) =>
+                isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
+            } to="/assignTasks">Assign Tasks</NavLink>
 
-        <NavLink className={({ isActive }) =>
-            isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
-        } to="/leaves">Leaves List</NavLink>
-        
-    </>
+            <NavLink className={({ isActive }) =>
+                isActive ? 'text-orange-500 font-semibold' : 'font-semibold hover:text-orange-500'
+            } to="/leaves">Leaves List</NavLink>
+
+        </>
 
     return (
         <div className="bg-slate-200 shadow-lg h-12 flex justify-around items-center">
             <h2 className="text-2xl font-semibold"><span className="text-orange-500">TODO</span> App</h2>
-            <div className="flex gap-5">{navLinks}</div>
+            <div className="flex gap-5">
+
+                {navLinks}
+                
+            </div>
         </div>
     );
 };
