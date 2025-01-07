f770e168583e3db043f1cd6be77c50b352e271c6
monwar23 <monwar2345@gmail.com>
2025-01-07 09:55:05 +0600

    added error page routing

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-6xl font-bold text-orange-500">404</h1>
             <p className="text-lg text-gray-700 mt-4">Oops! The page you are looking for does not exist.</p>
-            <a
-                href="/"
+            <Link
+                to='/'
                 className="mt-6 px-6 py-2 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-600 focus:outline-none"
             >
                 Go back to Home
-            </a>
+            </Link>
         </div>
     );
 };
